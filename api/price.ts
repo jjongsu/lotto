37b438db7c7c3ef6638a0f6f52a75b299ad948ed
hwangjongsu <hjs1000210002@gmail.com>
2025-11-09 13:10:27 +0900

    Text: string 체크

--- api/price.ts
+++ api/price.ts
@@ -16,13 +16,16 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
         const html = await response.text();
         const $ = cheerio.load(html);
-        const prizes: { rank: number; winAmount: number }[] = [];
+        const prizes: { rank: number; winAmount: number; winAmountStr1: string; winAmountStr2: string; winAmountStr3: string }[] = [];
 
         // ✅ "순위"는 직접 1~5 지정, "1게임당 당첨금액"은 td.eq(3)
         $('table.tbl_data tbody tr').each((i, el) => {
             const winAmountStr = $(el).find('td').eq(3).text().trim(); // "2,939,186,738원"
+            const winAmountStr1 = $(el).find('td').eq(3).text().trim().replace(/원/g, ''); // "2,939,186,738원"
+            const winAmountStr2 = $(el).find('td').eq(3).text().trim().replace(/,/g, ''); // "2,939,186,738원"
+            const winAmountStr3 = $(el).find('td').eq(3).text().trim().replace(/,/g, '').replace(/원/g, ''); // "2,939,186,738원"
             const winAmount = Number(winAmountStr.replace(/,/g, '').replace(/원/g, '')); // 숫자로 변환
-            prizes.push({ rank: i + 1, winAmount });
+            prizes.push({ rank: i + 1, winAmount, winAmountStr1, winAmountStr2, winAmountStr3 });
         });
 
         return res.status(200).json(prizes);

13b652756dfa0ef3bbb5a1752445a0b0e5f537a2
hwangjongsu <hjs1000210002@gmail.com>
2025-11-09 13:57:18 +0900

    Feat: data

--- src/hooks/useLottoData.ts
+++ src/hooks/useLottoData.ts
@@ -4,30 +4,51 @@ import { posLottoAmount } from '../apis/postLottoAmount';
 import { useMemo } from 'react';
 
 export default function useLottoData(drwNos: number[]) {
-    const amountsQueries = useQueries({
-        queries: drwNos.map((drwNo) => ({
-            queryKey: ['lotto', 'amount', drwNo],
-            queryFn: async () => posLottoAmount(drwNo),
-            enabled: !!drwNo, // drwNo가 존재할 때만 실행
-        })),
-    });
+    // const amountsQueries = useQueries({
+    //     queries: drwNos.map((drwNo) => ({
+    //         queryKey: ['lotto', 'amount', drwNo],
+    //         queryFn: async () => posLottoAmount(drwNo),
+    //         enabled: !!drwNo, // drwNo가 존재할 때만 실행
+    //     })),
+    // });
 
     const lottoQueries = useQueries({
-        queries: drwNos.map((num) => ({
-            queryKey: ['lotto', 'number', num],
-            queryFn: () => fetchLotto(num),
+        queries: drwNos.map((drwNo) => ({
+            queryKey: ['lotto', 'info', drwNo],
+            queryFn: async () => {
+                const priceRes = await fetch(`/api/price?drwNo=${drwNo}`);
+                if (import.meta.env.DEV) {
+                    const numRes = await fetch(`/lotto/common.do?method=getLottoNumber&drwNo=${drwNo}`);
+
+                    if (!numRes.ok || !priceRes.ok) throw new Error('Network error');
+                    const data = { ...numRes.json(), ...priceRes.json() };
+
+                    return data;
+                } else {
+                    const numRes = await fetch(`/api/lotto?drwNo=${drwNo}`);
+
+                    if (!numRes.ok || !priceRes.ok) throw new Error('Network error');
+                    const data = { ...numRes.json(), ...priceRes.json() };
+
+                    return data;
+                }
+            },
             enabled: !!drwNos.length,
         })),
     });
 
-    const isLoading = useMemo(() => amountsQueries.some((q) => q.isLoading) || lottoQueries.some((q) => q.isLoading), [amountsQueries, lottoQueries]);
-    const isError = useMemo(() => amountsQueries.some((q) => q.isError) || lottoQueries.some((q) => q.isError), [amountsQueries, lottoQueries]);
-    const data = lottoQueries.map(({ data }) => {
-        const targetAmount = amountsQueries.flatMap((amountData) => amountData.data).find((el) => el?.drwNo === data?.drwNo);
-        if (targetAmount) {
-            return { ...data, ...targetAmount };
-        }
-    });
+    const isLoading = lottoQueries.some((el) => el.isLoading);
+    const isError = lottoQueries.some((el) => el.isError);
+    const data = lottoQueries.map(({ data }) => data);
+
+    // const isLoading = useMemo(() => amountsQueries.some((q) => q.isLoading) || lottoQueries.some((q) => q.isLoading), [amountsQueries, lottoQueries]);
+    // const isError = useMemo(() => amountsQueries.some((q) => q.isError) || lottoQueries.some((q) => q.isError), [amountsQueries, lottoQueries]);
+    // const data = lottoQueries.map(({ data }) => {
+    //     const targetAmount = amountsQueries.flatMap((amountData) => amountData.data).find((el) => el?.drwNo === data?.drwNo);
+    //     if (targetAmount) {
+    //         return { ...data, ...targetAmount };
+    //     }
+    // });
 
     return { isLoading, isError, data };
 }
